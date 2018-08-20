6b07163b0bfb1a3725ff66a18505a6f82a660836
NicJM <nicolejeanne@users.noreply.github.com>
2018-08-20 00:12:35 -0500

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,59 @@ let min = 19;
 let max = 121;
 let crymin = 1;
 let crymax = 12;
+let randomCrystalOne = 0;
+let randomCrystalTwo = 0;
+let yourScoreTotal = 0;
+
+
+
+
+
+// Initializing the game
+function initializeGame(){
 
 // Generates random number between 19-120
 let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.querySelector(".random").innerHTML = randomNumber;
+$(".random").html(randomNumber);
 
 // Generates random number for crystals between 1-12
-var randomCrystal = Math.floor(Math.random() * (crymax - crymin + 1)) + crymin;
-    document.querySelector("#").innerHTML = randomCrystal;
+let randomCrystalOne = Math.floor(Math.random() * (crymax - crymin + 1)) + crymin;
+    $(".firstbutton").val(randomCrystalOne);
+let randomCrystalTwo = Math.floor(Math.random() * (crymax - crymin + 1)) + crymin;    
+    $(".secondbutton").val(randomCrystalTwo);
+let randomCrystalThree = Math.floor(Math.random() * (crymax - crymin + 1)) + crymin;    
+    $(".secondbutton").val(randomCrystalThree);
+let randomCrystalFour = Math.floor(Math.random() * (crymax - crymin + 1)) + crymin;    
+    $(".secondbutton").val(randomCrystalFour);
 
-//   function initializeGame(){
-    
-     
-//   initializeGame();
+// Add event handler to click event on each crystal
+$(".firstbutton").on("click", function() {
+    console.log(randomCrystalOne);
+    });
+   
+$(".secondbutton").on("click", function() {
+    console.log(randomCrystalTwo);
+   });
+
+$(".thirdbutton").on("click", function() {
+    console.log(randomCrystalThree);
+   });
+
+$(".fourthbutton").on("click", function() {
+    return (randomCrystalFour);
+   });  
+
+// Function for if/else statement to generate score
+
+// Win/Loss if/else
+if (yourScoreTotal === randomNumber){
+    wins++;
+} else if (yourScoreTotal > randomNumber){
+    losses++;
+}
+
+} 
+
+initializeGame();
 
  
